42262e89bc46e2dd7738207103d847372fd4fa62
Bransen Smith <95368430+bransensmith@users.noreply.github.com>
2022-01-12 16:59:05 -0500

    Create info.py

---
+++ info.py
@@ -0,0 +1,22 @@
+key_words = ['out of stock', 'sold out', 'not available']
+
+# BestBuy
+account_pass_bestbuy = ''
+account_email_bestbuy = ''
+
+personal_email = ''
+
+sign_in_link_bestbuy = 'https://www.bestbuy.com/identity/global/signin'
+location_link_bestbuy = 'https://www.bestbuy.com/site/store-locator'
+
+item_link_bestbuy = 'https://www.bestbuy.com/site/nvidia-geforce-rtx-3060-ti-8gb-gddr6-pci-express-4-0-graphics' \
+                    '-card' \
+                    '-steel-and-black/6439402.p?skuId=6439402'
+
+BestBuy_Link_Cart = 'https://www.bestbuy.com/cart'
+target_store_name = ''
+target_store_zip = ''
+
+gmail_username = ''
+gmail_app_pass = ''
+imap_url = 'imap.gmail.com'
