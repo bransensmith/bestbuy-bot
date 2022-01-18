8a3b1aeab3f631694788c00c1691c703436fbe56
Bransen Smith <95368430+bransensmith@users.noreply.github.com>
2022-01-18 14:26:32 -0500

    Update info.py

--- info.py
+++ info.py
@@ -1,23 +1,41 @@
-key_words = ['out of stock', 'sold out', 'not available']
 
-# BestBuy
+# BestBuy Account Info
 account_pass_bestbuy = ''
 account_email_bestbuy = ''
+
+
+# your first name on account 
 first_name = ''
 
+# your personal email to recieve alerts
 personal_email = ''
 
-sign_in_link_bestbuy = 'https://www.bestbuy.com/identity/global/signin'
-location_link_bestbuy = 'https://www.bestbuy.com/site/store-locator'
 
-item_link_bestbuy = 'https://www.bestbuy.com/site/nvidia-geforce-rtx-3060-ti-8gb-gddr6-pci-express-4-0-graphics' \
-                    '-card' \
-                    '-steel-and-black/6439402.p?skuId=6439402'
+# link to item you want to purchase on BestBuy.com (must be a Hot-Item)
+item_link_bestbuy = ''
 
-BestBuy_Link_Cart = 'https://www.bestbuy.com/cart'
+
+# name of your local BestBuy store (city name) and its Zip Code
 target_store_name = ''
 target_store_zip = ''
 
+
+# your gmail account username for outgoing alert system + app passwrod (different from normal password)
 gmail_username = ''
 gmail_app_pass = ''
+
+
+
+
+
+# dont chnage this for outgoing google domain!
 imap_url = 'imap.gmail.com'
+
+# do not change this list!
+key_words = ['out of stock', 'sold out', 'not available']
+
+# do not change these links!
+sign_in_link_bestbuy = 'https://www.bestbuy.com/identity/global/signin'
+location_link_bestbuy = 'https://www.bestbuy.com/site/store-locator'
+BestBuy_Link_Cart = 'https://www.bestbuy.com/cart'
+
