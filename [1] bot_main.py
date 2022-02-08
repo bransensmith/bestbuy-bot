ed312d9c1a62cb24ffcd250966ffd8e5ecb0400c
Bransen Smith <95368430+bransensmith@users.noreply.github.com>
2022-02-08 11:03:44 -0500

    Update [1] bot_main.py

--- [1] bot_main.py
+++ [1] bot_main.py
@@ -1,6 +1,6 @@
 if __name__ == '__main__':
     import main_bot_script
-    import main_bot_info as user_info
+    import user_info
 
     main_bot_script.user_info = user_info
     main_bot_script.main()
