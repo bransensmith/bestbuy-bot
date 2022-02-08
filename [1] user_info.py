41a23f52bd112450091b1794b85fc15697fdf5ef
Bransen Smith <95368430+bransensmith@users.noreply.github.com>
2022-02-08 10:58:36 -0500

    Create [1] user_info.py

---
+++ [1] user_info.py
@@ -0,0 +1,20 @@
+account_email_bestbuy = ''
+account_pass_bestbuy = ''
+
+first_name = ''
+
+personal_email = ''
+
+item_links_bestbuy = [
+    'https://www.bestbuy.com/site/pny-nvidia-geforce-rtx-3060-12gb-xlr8-gaming-revel-epic-x-rgb-single-fan-graphics-card/6454318.p?skuId=6454318',
+    'https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-8gb-gddr6-pci-express-4-0-graphics-card-dark-platinum-and-black/6429442.p?skuId=6429442',
+    'https://www.bestbuy.com/site/nvidia-geforce-rtx-3060-ti-8gb-gddr6-pci-express-4-0-graphics-card-steel-and-black/6439402.p?skuId=6439402',
+    'https://www.bestbuy.com/site/nvidia-geforce-rtx-3070-ti-8gb-gddr6x-pci-express-4-0-graphics-card-dark-platinum-and-black/6465789.p?skuId=6465789']
+
+
+target_store_name = ''
+target_store_zip = ''
+
+# reading emails
+personal_gmail_username = ''
+personal_gmail_app_pass = ''
